d4e357cbf391e635c190754c8b935ff0fb565713
OTO-KOMMON <wunjo94@gmail.com>
2022-02-21 17:34:43 +0900

    fix feedbacks

--- src/lib/FeatureDescriptionBox.svelte
+++ src/lib/FeatureDescriptionBox.svelte
@@ -48,48 +48,35 @@
 	</div>
 </div>
 <div class="bg-indigo-900 lg:py-16 pt-16 pb-2">
-	<div class="grid lg:grid-cols-12 js-show-on-scroll mb-8 justify-items-center">
-		<div
-			class="lg:col-start-1 lg:col-span-3 max-w-0 lg:max-w-lg lg:-ml-40 invisible lg:visible overflow-hidden"
-		>
+	<div class="grid lg:grid-cols-12 js-show-on-scroll mb-8">
+		<div class="lg:col-start-3 lg:col-span-3 lg:max-w-lg lg:text-left text-center">
 			<img
-				src="/landing-tide-2-3.png"
-				alt="tide gives meeting solution by setting agenda timelines and goals"
-			/>
-		</div>
-		<div class="lg:col-start-4 lg:col-span-6 lg:max-w-lg text-center">
-			<img
-				class="mb-12 invisible lg:visible max-w-0 lg:max-w-lg"
-				src="/landing-tide-2-1-changed.png"
-				alt="tide gives meeting solution by setting agenda timelines and goals"
-			/>
-			<img
-				class="mb-4 w-2/3 mx-auto"
+				class="mb-2 w-2/3 lg:mr-auto lg:ml-0 mx-auto"
 				src="/landing-tide-2-4.svg"
 				alt="tide gives meeting solution by setting agenda timelines and goals"
 			/>
-			<h2 class="text-4xl text-white">회의하다<br /><b>산으로 가는 일 없도록</b><br />••</h2>
+			<h2 class="text-4xl text-white">회의하다<br /><b>산으로 가는 일<br />없도록<br />••</b></h2>
 			<br />
 			<h2 class="text-2xl text-slate-100 font-light">
-				오늘 회의에서<br />어떤 순서로 무슨 이야기를 할 지<br />
+				오늘 회의에서<br />어떤 순서로<br />무슨 이야기를 할 지<br />
 			</h2>
-			<h2 class="text-2xl text-slate-100 mb-4">다같이 보며 이야기 해보세요</h2>
+			<h2 class="text-2xl text-slate-100 mb-6 lg:mb-4">다같이 보며<br />이야기 해보세요</h2>
 		</div>
-		<div class="lg:col-start-10 lg:col-span-2 lg:max-w-xl overflow-hidden">
+		<div class="lg:col-start-7 lg:col-span-4 text-center overflow-hidden">
 			<img
-				class="px-2 visible lg:hidden"
-				src="/landing-tide-2-1-changed.png"
+				class="inline-block px-4 lg:px-0 mb-2 lg:mb-4 mt-2 lg:mt-16"
+				src="/landing-tide-2-5.png"
 				alt="tide gives meeting solution by setting agenda timelines and goals"
 			/>
 			<img
-				class="relative px-2 lg:px-0 lg:left-24 lg:min-w-[400px] xl:min-w-[500px] h-auto overflow-hidden"
-				src="/landing-tide-2-2-changed.png"
+				class="mb-4 px-4 lg:px-0 lg:max-w-xl xl:max-w-2xl"
+				src="/landing-tide-2-6.png"
 				alt="tide gives meeting solution by setting agenda timelines and goals"
 			/>
 		</div>
 	</div>
 </div>
-<div class="grid lg:grid-cols-12 js-show-on-scroll mt-12 lg:pb-16 pb-2 mb-8 lg:shadow-lg">
+<div class="grid lg:grid-cols-12 js-show-on-scroll mt-12 lg:pb-16 pb-2 mb-4 lg:mb-0 lg:shadow-lg">
 	<div class="lg:col-start-3 lg:col-span-2 lg:text-left text-center lg:mb-24 mt-auto">
 		<h2 class="text-4xl text-zinc-800">
 			•••<br />회의할때<br /><b>메모할 공간이<br />필요하신가요?</b>
@@ -101,19 +88,19 @@
 	<div class="lg:col-start-5 lg:col-span-8 px-2 lg:px-0 overflow-hidden mt-4 lg:mt-0">
 		<img
 			class="relative overflow-hidden object-fit lg:left-20"
-			src="/landing-tide-3-changed.png"
+			src="/landing-tide-3-changed-2.png"
 			alt="tide gives meeting solution by setting agenda timelines and goals"
 		/>
 	</div>
 </div>
-<div class="py-2">
+<div class="bg-[url('/landing-tide-6.jpg')] bg-cover lg:py-2 py-8">
 	<div class="grid lg:grid-cols-12 js-show-on-scroll">
 		<div
-			class="lg:col-start-4 lg:col-span-6 text-center lg:mb-24 mb-8 lg:mt-12 mt-0 lg:p-12 p-4 border-t-zinc-400"
+			class="lg:col-start-4 lg:col-span-6 text-center lg:mb-24 mb-8 lg:mt-24 mt-0 lg:p-12 p-4 border-t-zinc-400"
 		>
-			<h2 class="text-2xl text-zinc-800 font-light ">
+			<h2 class="text-2xl text-white font-light ">
 				회의 전용 솔루션으로<br />팀의 회의시간 대비 효율을<br class="lg:hidden" /> 극대화하세요<br
-				/><b class="text-cyan-800">바쁜 팀일수록 필요합니다</b>
+				/><b class="text-cyan-200">바쁜 팀일수록 필요합니다</b>
 			</h2>
 			<button
 				class="transition-all duration-150 ease-out px-8 py-2 bg-cyan-600 rounded-full text-xl text-white mt-6 hover:bg-cyan-800"
